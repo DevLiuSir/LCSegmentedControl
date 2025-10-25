d37a223ac58414e296839ba038b8fb43ddc8e4a3
Marvin <93428739@qq.com>
2025-10-25 15:44:36 +0800

    add  demo

---
+++ Demo/LCSegmentedControlDemo/LCSegmentedControlDemo/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  LCSegmentedControlDemo
+//
+//  Created by Liu Chuan on 2025/10/25.
+//
+//  Copyright Ningbo Shangguan Technology Co.,Ltd. All Rights Reserved.
+//  宁波上官科技有限公司版权所有，保留一切权利。
+//
+    
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Demo/LCSegmentedControlDemo/LCSegmentedControlDemo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  LCSegmentedControlDemo
+//
+//  Created by Liu Chuan on 2025/10/25.
+//
+//  Copyright Ningbo Shangguan Technology Co.,Ltd. All Rights Reserved.
+//  宁波上官科技有限公司版权所有，保留一切权利。
+//
+    
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
